c31c7613da551a85a5c0772c301857867650cdf2
Jesse Pai <jessepai@google.com>
2023-02-28 12:48:44 -0800

    Add better keymappings and configuration for terminal

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,3 +1,27 @@
-require('toggleterm').setup()
+require('toggleterm').setup({
+  shade_filetypes = { 'none' },
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 30
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end
+})
+
+local set_terminal_keymaps = function()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = 'term://*',
+  callback = set_terminal_keymaps,
+})
 
 return {}
