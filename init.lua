73bab6d89311c8244237cf4a12ded134d13085e5
Jesse Pai <jessepai@google.com>
2026-02-04 14:10:49 -0800

    Add support for ciderlsp.

--- init.lua
+++ init.lua
@@ -761,25 +761,71 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'lua_ls', -- Lua Language server
         'stylua', -- Used to format Lua code
+        -- You can add other tools here that you want Mason to install
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      local citc = require('lspconfig').util.root_pattern('.citc')
+
+      for name, server in pairs(servers) do
+        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+        vim.lsp.config(name, server)
+        if citc(vim.api.nvim_buf_get_name(0)) == nil then
+          vim.lsp.enable(name)
+        end
+      end
+
+      -- Special Lua Config, as recommended by neovim help docs
+      vim.lsp.config('lua_ls', {
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              version = 'LuaJIT',
+              path = { 'lua/?.lua', 'lua/?/init.lua' },
+            },
+            workspace = {
+              checkThirdParty = false,
+              -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
+              --  See https://github.com/neovim/nvim-lspconfig/issues/3189
+              library = vim.api.nvim_get_runtime_file('', true),
+            },
+          })
+        end,
+        settings = {
+          Lua = {},
         },
-      }
-    end,
+      })
+      if citc(vim.api.nvim_buf_get_name(0)) == nil then
+        vim.lsp.enable 'lua_ls'
+      end
+
+      vim.lsp.config('ciderlsp', {
+        cmd = {
+          "/google/bin/releases/cider/ciderlsp/ciderlsp",
+          "--tooltag=neovim-lsp",
+          "--noforward_sync_responses",
+        },
+        filetypes = { "c", "cpp", "java", "kotlin", "objc", "proto", "textpb", "go", "python", "bzl" },
+        root_dir = function(bufnr, cb)
+          local fname = vim.api.nvim_buf_get_name(bufnr)
+          local root_dir = "/google"
+          if vim.startswith(fname, root_dir) then
+            cb(root_dir)
+          end
+        end,
+      })
+      if citc(vim.api.nvim_buf_get_name(0)) ~= nil then
+        vim.lsp.enable 'ciderlsp'
+      end
+
+      end,
   },
 
   { -- Autoformat
