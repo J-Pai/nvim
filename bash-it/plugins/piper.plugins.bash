7c6a748d62438659705249fe5e1b771df387afc8
Jesse Pai <jessepai@google.com>
2026-04-24 16:22:17 -0700

    Add commandline info for piper using jj.

--- bash-it/plugins/piper.plugins.bash
+++ bash-it/plugins/piper.plugins.bash
@@ -7,7 +7,7 @@ SCM_CITC='citc'
 SCM_CITC_CHAR='G3±'
 SCM_CITC_DETACHED_CHAR='⌿'
 SCM_CITC_AHEAD_CHAR="↑"
-SCM_CITC_BEHIND_CHAR="↓"
+SCM_CITC_NON_UPLOADED_CHAR="⇧"
 SCM_CITC_AHEAD_BEHIND_PREFIX_CHAR=" "
 SCM_CITC_UNTRACKED_CHAR="?:"
 SCM_CITC_UNSTAGED_CHAR="U:"
@@ -45,25 +45,103 @@ function scm_prompt_char() {
 	esac
 }
 
+function cl_linkify() {
+	local base_url="${1}"
+
+	if [[ $base_url =~ ^@?(cl/[0-9]+) ]]; then
+		base_url="${BASH_REMATCH[1]}"
+		echo -n "\e]8;;http://${base_url}\a${1}\e]8;;\a"
+	else
+		echo -n "${1}"
+	fi
+}
+
 function citc_prompt_vars() {
-	local clickable_prefix="\e]8;;http://"
-	local clickable_separator="\a"
-	local clickable_suffix="\e]8;;"
-	local jj_vars=$(jj log\
-		--no-graph \
-		-r @ \
-		-T \
-		"join(',', bookmarks,parents.first().bookmarks())")
-	IFS="," read -ra jj_vars_array <<< "${jj_vars}"
+	local start=$EPOCHREALTIME
+	local jj_query="$(jj log --no-graph -r p4head::@ -T '
+if(current_working_copy,
+  join(",",
+    if(bookmarks.len() > 0,
+      "@" ++ bookmarks.first(),
+      if(parents.first().p4head() && empty,
+        "cl/" ++ parents.first().submitted_change_number() ++ "(p4head)",
+        if(parents.first().bookmarks().len() > 0,
+          parents.first().bookmarks().first(),
+          "cl/*"
+        )
+      )
+    ),
+    diff.files().len(),
+    conflicted_files.len(),
+  ),
+  if(bookmarks.len() > 0,
+    "-",
+    if(p4head,
+      "",
+      "^"
+    )
+  )
+)
+')"
+	if [[ "${CITC_BENCHMARK}" != "" ]]; then
+		local duration="$(echo "scale=2; ($EPOCHREALTIME - $start)" | bc)s "
+	fi
+	local non_uploaded_ahead=$(echo "${jj_query}" |  tr -cd '^' | wc -c)
+	local ahead=$(( $(echo "${jj_query}" |  tr -cd '-' | wc -c) + $non_uploaded_ahead ))
+	jj_query=${jj_query//-/}
+	jj_query=${jj_query//^/}
+	IFS=',' read -r -a jj_query_array <<< "${duration},${jj_query}"
+
+	local duration="${jj_query_array[0]}"
+	local cl="${jj_query_array[1]}"
+	local open_files="${jj_query_array[2]}"
+	local unstaged_files="0"
+	local conflicted_files="${jj_query_array[3]}"
+
+	if [[ "${cl}" == "cl/*" ]]; then
+		unstaged_files="${open_files}"
+		open_files="0"
+	fi
+
 	SCM_BRANCH="$(basename $(jj workspace root))"
-	SCM_CHANGE=" ${clickable_prefix}${jj_vars_array[1]}${clickable_separator}${jj_vars_array[1]}${clickable_suffix}${clickable_separator}"
-	SCM_DIRTY=2
-	SCM_STATE="${CITC_THEME_PROMPT_CLEAN:-${SCM_THEME_PROMPT_CLEAN:-}}"
+
+	if [[ "${ahead}" -gt 0 ]]; then
+		SCM_BRANCH+="${SCM_CITC_AHEAD_BEHIND_PREFIX_CHAR}${SCM_CITC_AHEAD_CHAR}"
+		SCM_BRANCH+="${ahead}"
+	fi
+
+	if [[ "${non_uploaded_ahead}" -gt 0 ]]; then
+		SCM_BRANCH+="${SCM_CITC_AHEAD_BEHIND_PREFIX_CHAR}${SCM_CITC_NON_UPLOADED_CHAR}"
+		SCM_BRANCH+="${non_uploaded_ahead}"
+	fi
+
+	if [[ "${open_files}" -gt 0 || "${unstaged_files}" -gt 0 || "${conflicted_files}" -gt 0 ]]; then
+		SCM_DIRTY=1
+		if [[ "${open_files}" -gt 0 ]]; then
+			SCM_BRANCH+=" ${SCM_CITC_STAGED_CHAR}${open_files}" && SCM_DIRTY=3
+		fi
+
+		if [[ "${unstaged_files}" -gt 0 ]]; then
+			SCM_BRANCH+=" ${SCM_CITC_UNSTAGED_CHAR}${unstaged_files}" && SCM_DIRTY=2
+		fi
+
+		if [[ "${conflicted_files}" -gt 0 ]]; then
+			SCM_BRANCH+=" ${SCM_CITC_UNTRACKED_CHAR}${conflicted_files}" && SCM_DIRTY=1
+		fi
+
+
+		SCM_STATE="${CITC_THEME_PROMPT_DIRTY:-${SCM_THEME_PROMPT_DIRTY?}}"
+	fi
+
+
 	SCM_PREFIX="${CITC_THEME_PROMPT_PREFIX:-${SCM_THEME_PROMPT_PREFIX-}}"
 	SCM_SUFFIX="${CITC_THEME_PROMPT_SUFFIX:-${SCM_THEME_PROMPT_SUFFIX-}}"
+
+	SCM_CHANGE=" ${duration}$(cl_linkify ${cl})"
 }
 
 function citc_prompt_info() {
 	citc_prompt_vars
-	echo -ne "${SCM_PREFIX?}${SCM_BRANCH?}:${SCM_CHANGE?}${SCM_STATE?}${SCM_SUFFIX?}"
+
+	echo -ne "${SCM_PREFIX?}${SCM_BRANCH?}${SCM_CHANGE?}${SCM_STATE?}${SCM_SUFFIX?}"
 }
