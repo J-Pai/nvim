83064c74e187b78907487dad7449416e74de821a
Jesse Pai <jessepai@google.com>
2026-07-06 10:36:43 -0700

    scl = bzl.

--- init.lua
+++ init.lua
@@ -241,6 +241,13 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   end,
 })
 
+-- Filetype mappings.
+vim.filetype.add({
+  extension = {
+    scl = 'bzl'
+  },
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
